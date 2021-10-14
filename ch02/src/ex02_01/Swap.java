3f335c66dd157732478fdb94149e81e4090569af
Abyul <kinsng1599@gmail.com>
2021-10-15 00:58:31 +0900

    swap 메소드

---
+++ ch02/src/ex02_01/Swap.java
@@ -0,0 +1,20 @@
+package ex02_01;
+
+import java.util.Arrays;
+
+public class Swap {
+	static void swap(int[] a, int idx1, int idx2) {
+		int tmp = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = tmp;
+	}
+	
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4};
+		
+		swap(arr,0,3);
+		
+		System.out.println(Arrays.toString(arr));
+		// output : [4, 2, 3, 1]
+	}
+}
