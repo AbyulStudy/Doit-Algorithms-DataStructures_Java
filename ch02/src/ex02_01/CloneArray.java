f728df1c4628fd5572f64fea55574ce34c511fe9
Abyul <kinsng1599@gmail.com>
2021-10-14 23:42:43 +0900

    배열의 복제(클론) .clone() 메소드 사용

--- ch02/src/ex02_01/CloneArray.java
+++ ch02/src/ex02_01/CloneArray.java
@@ -22,5 +22,8 @@ public class CloneArray {
 			cloneArr =  1 2 3 0 5
 		 					  ↑
 		 */
+		
+		// 배열 cloneArr 은 score 배열의 복제를 참조 하도록 초기화 했습니다.
+		// cloneArr[3] 을 바꾼뒤 모든 요소값을 확인 했을때 배열변수 cloneArr의 참조가 score배열의 본체 그 자체가 아니라 그 복제임을 확인 할수 있습니다.
 	}
 }
