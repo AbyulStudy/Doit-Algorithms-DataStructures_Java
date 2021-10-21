e7af92e5f028961eb96b9251f6c33b6faf3fab2c
Abyul <kinsng1599@gmail.com>
2021-10-21 21:31:47 +0900

    new 생성자로 배열 생성

--- ch02/src/ex02_01/IntArray.java
+++ ch02/src/ex02_01/IntArray.java
@@ -1,5 +1,5 @@
 package ex02_01;
-// 구성 요소의 자료형이 int형인 배열 (구성 요솟수는 5 : new에 의해 본체를 생성)
+// 구성 요소의 자료형이 int형인 배열 (구성 요솟수는 5 : new에 의해 본체를 생성) 
 public class IntArray {
 	public static void main(String[] args) {
 		int[] score = new int[5]; // 배열의 선언
